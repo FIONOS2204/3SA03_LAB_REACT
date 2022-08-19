6366e5d187e017c6c7b9767e90119a60f53f2016
Grirkrit phiphatsombat <nut220445@gmail.com>
2022-08-19 10:32:04 +0700

    Part 5

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 export default function CharacterCard(props) {
     return (
-        <div className='card'>{props.value}</div>
+        <div className='card'>
+            {props.value}
+        </div>
     )
 }
